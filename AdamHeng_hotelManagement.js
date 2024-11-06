a2e267578081b39fde7d933287b7f10838f6e691
adamhyd <adamheng69@gmail.com>
2024-11-06 11:42:35 +0800

    done 5 functions and started on app

--- AdamHeng_hotelManagement.js
+++ AdamHeng_hotelManagement.js
@@ -1,12 +1,13 @@
 // DUMMY DATA FOR ROOMS
 const rooms = [
-    { roomID: '1', floor: '2', status: 'Available', pricePerNight: 250, description: 'Premium Twin Room', guestID: null },
-    { roomID: '2', floor: '6', status: 'Occupied', pricePerNight: 112, description: 'Standard Single Room', guestID: '500' },
-    { roomID: '3', floor: '5', status: 'Maintenance', pricePerNight: 212, description: 'Standard Twin Room', guestID: null },
-    { roomID: '4', floor: '1', status: 'Available', pricePerNight: 900, description: 'Deluxe Suite with City View', guestID: null },
-    { roomID: '5', floor: '3', status: 'Occupied', pricePerNight: 400, description: 'Deluxe Double Room', guestID: '501' },
+    { roomID: '1', floor: '2', status: 'Available', pricePerNight: 250, description: 'Premium Twin Room', guestID: null, cleaningSchedule: null },
+    { roomID: '2', floor: '6', status: 'Occupied', pricePerNight: 112, description: 'Standard Single Room', guestID: '500', cleaningSchedule: null },
+    { roomID: '3', floor: '5', status: 'Maintenance', pricePerNight: 212, description: 'Standard Twin Room', guestID: null, cleaningSchedule: null },
+    { roomID: '4', floor: '1', status: 'Available', pricePerNight: 900, description: 'Deluxe Suite with City View', guestID: null, cleaningSchedule: null },
+    { roomID: '5', floor: '3', status: 'Occupied', pricePerNight: 400, description: 'Deluxe Double Room', guestID: '501', cleaningSchedule: null },
 ]
 
+//DUMMY DATA FOR CLEANERS
 const cleaners = [
     { cleanerID: '100', status: 'Busy'},
     { cleanerID: '101', status: 'Available'},
@@ -28,15 +29,28 @@ module.exports = {
     // This assignRoomCleaningService() function assigns room cleaning service with the cleaner's ID and cleaning date for the cleaner.
     assignRoomCleaningService(roomID, cleanerID, cleaningDate) {
         const room = rooms.find(room => room.roomID === roomID);
-        const cleaner = cleaners.find(cleaner => cleaners.cleanerID === cleanerID);
+        const cleaner = cleaners.find(cleaners => cleaners.cleanerID === cleanerID);
 
         if (!room) {
-            return `Room ${roomID} is not found.`;
+            return `Room ${roomID} could not be found.`;
         }
 
-        
+        if (!cleaner) {
+            return `Cleaner ${cleanerID} could not be found.`;
+        }
+
+        if (cleaner.status === 'Busy') {
+            return `Cleaner ${cleanerID} is currently busy and cannot be assigned to cleaning duty.`
+        } 
+
+        if (cleaningSchedule = null) {
+            cleaner.status = 'Busy';
+            room.cleaningSchedule = {cleanerID, cleaningDate};
+        }
+
+
     },
-    // This bookRoomForGuest() function books a room for a guest
+    // This bookRoomForGuest() function books a room for a guest under the conditions that the room is not occupied or under maintenance.
     bookRoomForGuest(roomID, guestID) {
         const room = rooms.find(room => room.roomID === roomID);
 
@@ -44,8 +58,8 @@ module.exports = {
             return `Room ${roomID} is not found.`;
         }
 
-        if (room.status == 'Occupied') {
-            return `Room ${roomID} is currently ${roomID.status}`;
+        if (room.status == 'Occupied' || room.status == 'Maintenance') {
+            return `The room status for Room ${roomID} is currently ${roomID.status}, and it cannot be booked.`;
         } else {
             return `Room ${roomID} has been succesfully booked for Guest ${guestID}.`;
         }
@@ -82,6 +96,10 @@ module.exports = {
             return `Room ${roomID} is not found.`;
         }
 
-        return `Room ${roomID} status is ${roomID.status}`;
+        return `Room ${roomID} status is ${roomID.status}.`;
     },
+    //This function shows all the rooms that are currently available for guests to book.
+    retrieveAllAvailableRooms() {
+
+    }
 }
---
+++ app.js
@@ -0,0 +1,13 @@
+//call the functions here to the adamheng_hotelmanagement.js'
+//similar to lab 2
+
+const hotelManagement = require("./AdamHeng_hotelManagement.js");
+
+console.log("Starting Hotel Management System!");
+
+//Dummy data to use in the functions
+const roomID = 1;
+const guestID = 401;
+const nights = 4;
+const cleanerID = '101'; //This cleaner should be busy
+const cleaningDate =  '05-11-2024';
